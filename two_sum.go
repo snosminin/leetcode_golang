67c7b90b20b6474b385e5b2234e9ea3e7ef6ee2b
soul1on1fire <soul1on1fire@gmail.com>
2025-05-15 03:23:24 -0700

    Add two sum example

---
+++ two_sum.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+var ARRAY = [...]int{2, 7, 11, 15, 34, 2, 1, 6, 8, 4, 234, 2, 14, 7, 0}
+var SUM_TO_SEARCH = 25
+
+func main() {
+	fmt.Println(twoSumSearch(ARRAY[:], SUM_TO_SEARCH))
+}
+
+func twoSumSearch(nums []int, target int) []int {
+	var resultMap = make(map[int]int)
+	for i, item := range nums {
+		var diff = target - item
+		_, ok := resultMap[diff]
+		if diff > 0 && ok {
+			return []int {i, resultMap[diff]}
+		}
+		resultMap[item] = i
+	}
+	return nil
+}
