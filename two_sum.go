20141129c666cdf81b014b5de560c55763c48a5f
soul1on1fire <soul1on1fire@gmail.com>
2025-05-15 04:08:59 -0700

    Fix two sum

--- two_sum.go
+++ two_sum.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-var ARRAY = [...]int{2, 7, 11, 15, 34, 2, 1, 6, 8, 4, 234, 2, 14, 7, 0}
-const SUM_TO_SEARCH = 25
-
 func main() {
-	fmt.Println(twoSumSearch(ARRAY[:], SUM_TO_SEARCH))
+	var array = [...]int{2, 7, 11, 15, 34, 2, 1, 6, 8, 4, 234, 2, 14, 7, 0}
+	const sumToSearch = 25
+
+	fmt.Println(twoSumSearch(array[:], sumToSearch))
 }
 
 func twoSumSearch(nums []int, target int) []int {
