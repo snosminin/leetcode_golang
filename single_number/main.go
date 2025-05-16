8b6307d81920ca95cfaa0eca6dc2d81e8185deaf
Sergei <>
2025-05-16 18:58:39 +0600

    Add single number

--- single_number/main.go
+++ single_number/main.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var array = [...]int{4,1,2,1,2}
+	var array = [...]int{2, 2, 1}
 
 	fmt.Println(singleNumber(array[:]))
 }
 
 func singleNumber(nums []int) int {
-	
+	var result = 0
+	for _, num := range nums {
+		result ^=num
+	}
+	return result
 }
