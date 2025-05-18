c95e579c0f4e74b9beee1547645d5613d0f47254
Sergei <>
2025-05-18 18:59:26 +0600

    Add move zeroes

---
+++ move_zeroes/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var array = [...]int{0,1,0,3,12}
+	const sumToSearch = 25
+	moveZeroes(array[:])
+	fmt.Println(array)
+}
+
+func moveZeroes(nums []int)  {
+	if(len(nums) == 0) {
+		return
+	}
+	
+	for range len(nums) - 1 {
+		for j := range len(nums) - 1 {
+			if(nums[j] == 0) {
+				swap := nums[j+1]
+				nums[j+1] = nums[j]
+				nums[j] = swap
+			}
+		}
+	}
+}
