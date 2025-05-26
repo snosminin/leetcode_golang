a2d75eca2abe5877f4427a7ea08622550b21f477
Sergei <>
2025-05-26 19:17:07 +0600

    Add majority element

---
+++ majority_element/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var array = [...]int{2, 7, 2, 15, 34, 2, 1, 6, 2, 4, 2, 2, 2, 7, 2}
+
+	fmt.Println(majorityElement(array[:]))
+}
+
+func majorityElement(nums []int) int {
+    candidate := -1
+	count := 0
+	for _, num := range nums {
+		if(count == 0) {
+			candidate = num
+		}
+		if (num == candidate) {
+			count++
+		} else {
+			count--
+		}
+	}
+	return candidate
+}
