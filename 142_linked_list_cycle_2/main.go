70bf36ac20bb365308a95d0307bc48da2e8d5756
Sergei <>
2025-06-01 00:04:45 +0600

    Fix 142 linked list cycle 2

--- 142_linked_list_cycle_2/main.go
+++ 142_linked_list_cycle_2/main.go
@@ -8,12 +8,12 @@ type ListNode struct {
 }
 
 func main() {
-	var l8 = ListNode{Val: 1, Next: nil}
-	var l7 = ListNode{Val: 1, Next: &l8}
-	var l6 = ListNode{Val: 2, Next: &l7}
-	var l5 = ListNode{Val: 2, Next: &l6}
-	var l4 = ListNode{Val: 2, Next: &l5}
-	var l3 = ListNode{Val: 2, Next: &l4}
+	var l8 = ListNode{Val: 8, Next: nil}
+	var l7 = ListNode{Val: 7, Next: &l8}
+	var l6 = ListNode{Val: 6, Next: &l7}
+	var l5 = ListNode{Val: 5, Next: &l6}
+	var l4 = ListNode{Val: 4, Next: &l5}
+	var l3 = ListNode{Val: 3, Next: &l4}
 	var l2 = ListNode{Val: 2, Next: &l3}
 	var l1 = ListNode{Val: 1, Next: &l2}
 	l8.Next = &l5
@@ -35,6 +35,11 @@ func detectCycle(head *ListNode) *ListNode {
 		}
 		currentDoubleStep = currentDoubleStep.Next
 		if(current == currentDoubleStep){
+			currentDoubleStep = head
+			for current != currentDoubleStep{
+				current = current.Next
+				currentDoubleStep = currentDoubleStep.Next
+			}
 			return current
 		}
 	}
