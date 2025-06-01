8090ead74a26d1bac6909399bb524da5caf4546e
Sergei <>
2025-06-01 19:20:35 +0600

    Add 104 maximum depth of binary tree

---
+++ 104_maximum_depth_of_binary_tree/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+		Val int
+		Left *TreeNode
+		Right *TreeNode
+	}
+
+func main() {
+	fmt.Println(maxDepth(nil))
+}
+
+func maxDepth(root *TreeNode) int {
+	if(root == nil) {
+		return 0
+	}
+
+	leftDepth:= maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+
+    return max(leftDepth, rightDepth) + 1
+}
