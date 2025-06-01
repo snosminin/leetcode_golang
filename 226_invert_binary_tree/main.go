7927c227bc5fd41bb49e370e2eb52e6a8fecd418
Sergei <>
2025-06-01 20:20:51 +0600

    Change 226 invert binary tree

--- 226_invert_binary_tree/main.go
+++ 226_invert_binary_tree/main.go
@@ -9,23 +9,17 @@ type TreeNode struct {
 	}
 
 func main() {
-	fmt.Println(isSymmetric(nil))
+	fmt.Println(invertTree(nil))
 }
 
-func isSymmetric(root *TreeNode) bool {
-    return checkSymmetry(root, root)
-}
-
-func checkSymmetry(node1 *TreeNode, node2 *TreeNode) bool {
-	if(node1 == nil && node2 == nil) {
-		return true
-	}
-	if(node1 == nil) != (node2 == nil) {
-		return false
-	}
-	if(node1.Val != node2.Val){
-		return false
+func invertTree(root *TreeNode) *TreeNode {
+	if(root == nil) {
+		return nil
 	}
 
-	return checkSymmetry(node1.Left, node2.Right) && checkSymmetry(node1.Right, node2.Left)
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+
+    return root
 }
