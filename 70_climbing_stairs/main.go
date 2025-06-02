209ae6cf493298302f0ca46f32c40245615f9c08
Sergei <>
2025-06-02 18:58:12 +0600

    Add 70 climbing stairs

---
+++ 70_climbing_stairs/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(climbStairs(2))
+}
+
+func climbStairs(n int) int {
+    previous1 := 1
+	previous2 := 1
+
+	for i := 2; i <= n; i++ {
+		current := previous1 + previous2
+		previous2 = previous1
+		previous1 = current
+	}
+
+	return previous1
+}
