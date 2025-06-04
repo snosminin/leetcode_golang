de2ebcd89e2e88e7176360342ea4ab92b32f8604
Sergei <>
2025-06-04 20:37:20 +0600

    Add 3 longest substring without repeating characters

---
+++ 3_longest_substring_without_repeating_characters/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "abcabcbb"
+	
+	fmt.Println(lengthOfLongestSubstring(s))
+}
+
+func lengthOfLongestSubstring(s string) int {
+	lastIndexMap := make(map[rune]int)
+
+	start := 0
+	maxLength := 0
+	currentLength := 0
+	for end, c := range s {
+		lastIndex, ok := lastIndexMap[c]
+		if(!ok) {
+			lastIndex = -1
+		}
+		start = max(start,lastIndex+1)
+
+		currentLength = end - start + 1
+		maxLength = max(currentLength, maxLength)
+
+		lastIndexMap[c] = end
+	}
+
+	return maxLength
+}
