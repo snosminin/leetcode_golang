feb99c2556f161be59321c1901371049ad58499a
Sergei <>
2025-06-06 14:32:20 +0600

    Add 189 rotate array

---
+++ 189_rotate_array/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+		Val int
+		Left *TreeNode
+		Right *TreeNode
+	}
+
+func main() {
+	nums, val := []int{}, 0
+	rotate(nums, val)
+	fmt.Println(nums, val)
+}
+
+func rotate(nums []int, k int) {
+	if(len(nums) == 0) {
+		return
+	}
+	
+	k = k % len(nums)
+	result := append(nums[len(nums)-k:], nums[:len(nums)-k]...)
+	for i, _ := range nums {
+		nums[i] = result[i]
+	}
+}
