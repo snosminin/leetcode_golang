81b878914271ff4c429c594c082853f3c2136459
Sergei <>
2025-06-06 20:08:48 +0600

    Add 392 is subsequence

---
+++ 392_is_subsequence/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	s, t := "axc", "ahbgdc"
+
+	fmt.Println(isSubsequence(s, t))
+}
+
+func isSubsequence(s string, t string) bool {
+	sIndex, tIndex := 0, 0
+
+	for sIndex < len(s) && tIndex < len(t) {
+		if(s[sIndex] == t[tIndex]){
+			sIndex++
+		}
+		tIndex++
+	}
+
+	return sIndex == len(s)
+}
