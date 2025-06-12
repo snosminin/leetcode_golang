c325478bd1526ae308dfeffbf3467573c79be40e
Sergei <>
2025-06-12 21:42:48 +0600

    Add 26 remove duplicates from sorted array

---
+++ 26_remove_duplicates_from_sorted_array/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var array = []int{1,2}
+
+	fmt.Println(removeDuplicates(array))
+}
+
+func removeDuplicates(nums []int) int {
+	if len(nums) <= 2 {
+		return len(nums)
+	}
+
+	index := 2
+	for i := 2; i < len(nums); i++ {
+		if nums[index-1] == nums[i] && nums[index-2] != nums[i] {
+			nums[index] = nums[i]
+			index++
+		} else if nums[index-1] < nums[i] {
+			nums[index] = nums[i]
+			index++
+		}
+	}
+	return index
+}
