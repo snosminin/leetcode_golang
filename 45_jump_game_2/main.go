4774c46364229bb9188313918406d3fc0a706216
Sergei <>
2025-06-16 19:11:26 +0600

    Add 45 jump game 2

---
+++ 45_jump_game_2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var array = []int{2,3,1,1,4}
+
+	fmt.Println(jump(array))
+}
+
+func jump(nums []int) int {
+	if(len(nums) == 1){
+		return 0
+	}
+
+	result, farthest, end := 0, 0, 0
+
+	for i := range nums {
+		if farthest < i + nums[i] {
+			farthest = i + nums[i]
+		}
+		
+		if farthest >= len(nums)-1 {
+			result++
+			break
+		}
+
+		if i == end {
+			result++
+			end = farthest
+		}
+	}
+
+	return result
+}
